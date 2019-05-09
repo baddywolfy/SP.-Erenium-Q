8c1b57283e691c0d46d2c67b3828da241160b597
baddywolfy <47282162+baddywolfy@users.noreply.github.com>
2019-05-09 10:59:42 +0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var nav = document.querySelector('nav'); // Identify target
+
+window.addEventListener('scroll', function(event) { // To listen for event
+    event.preventDefault();
+
+    if (window.scrollY <= 150) { // Just an example
+        nav.style.backgroundColor = 'transparent'; // or default color
+    } else {
+        nav.style.backgroundColor = '#141516';
+    }
+});
+
+var el = $('.choose-us-list');
+
+$(window).scroll(function(){
+    if ( $(this).scrollTop() > el.offset().top - 1130 ) {
+        el.addClass('slideUp');
+    }
+});
+
+$(function(){
+  $('.single-slide').slick();
+});
+
+$(document).ready(function(){
+  $('.menu_burger').click(function(){
+    $('ul').toggleClass('menu_active');
+  })
+})
